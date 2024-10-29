fdc14567d88a1b1f377b0a3b17f02f3955ac422c
codechrononaut <tyler.frink@student.northampton.edu>
2024-10-29 20:01:39 +0000

    Assignment2

---
+++ Assignment2/Assignment2_Frink.cpp
@@ -0,0 +1,19 @@
+//book series enter program 
+#include<iostream>
+using namespace std;
+int main (){
+    //these are the data types used to declare variables 
+    string title;
+    string authorsname;
+    float price;
+    //input and output for all variables via cout
+cout<<"enter book";
+cin>>title;
+cout<<"enter authorsname";
+cin>>authorsname; 
+cout<<"enter price";
+cin>>price;
+cout<<"booktitle is "<<title<<endl;
+cout<<"authors name is "<<authorsname<<endl;
+cout<<"the price is"<<price<<endl;
+}
