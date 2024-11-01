4ad3f69c67e7cc1b4e814462007c5c4c0dbc03e8
Thu2k81 <kaungmyatthu.2k81@gmail.com>
2024-11-01 22:59:38 +0000

    Done

---
+++ Assignment 4/InClassAssignment2_Thu.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Set the initial value 20 and decrement by 2 until i is equal to 2
+    for (int i = 20; i >= 2; i = i - 2){ 
+        cout << " " << i;
+    }
+    return 0;
+}
